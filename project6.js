cc289fb26283a694dec5be18c843dcb0017a921e
charlesmadu <100036347+charlesmadu@users.noreply.github.com>
2022-02-19 18:45:02 +0000

    Added Javascript

---
+++ project6.js
@@ -0,0 +1,76 @@
+const breakfast_array = [
+    {
+        "name": "Buttermilk Pancakes",
+        "price": 15.99,
+        "text": "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+        "image": "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-1.jpeg"
+    },
+    {
+        "name": "Country Delight",
+        "price": 20.99,
+        "text": "Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut",
+        "image": "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-4.jpeg"
+    },
+    {
+        "name": "Bacon Overflow",
+        "price": 8.99,
+        "text": "carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird",
+        "image": "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-7.jpeg"
+    }
+
+];
+
+const lunch_array = [
+    {
+        "name": "Diner Double",
+        "price": 13.99,
+        "text": "vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats",
+        "image": "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-2.jpeg"
+    },
+    {
+        "name": "Egg Attack",
+        "price": 22.99,
+        "text": "franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up",
+        "image": "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-5.jpeg"
+    },
+    {
+        "name": "American Classic",
+        "price": 12.99,
+        "text": "on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut",
+        "image": "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-8.jpeg"
+    }
+
+];
+
+const shakes_array = [
+    {
+        "name": "Godzilla Milkshake",
+        "price": 6.99,
+        "text": "ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.",
+        "image": "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-3.jpeg"
+    },
+    {
+        "name": "Oreo Dream",
+        "price": 18.99,
+        "text": "Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday",
+        "image": "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-6.jpeg"
+    },
+    {
+        "name": "Quarantine Buddy",
+        "price": 16.99,
+        "text": "skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.",
+        "image": "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-9.jpeg"
+    }
+
+];
+
+const shakes_array = [
+    {
+        "name": "Steak Dinner",
+        "price": 39.99,
+        "text": "skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.",
+        "image": "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-10.jpeg"
+    }
+];
+
+
